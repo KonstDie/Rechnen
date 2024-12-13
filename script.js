d79b6626bbec167b62f2c170920bdf65f0e4103b
KonstDie <79140815+KonstDie@users.noreply.github.com>
2024-12-13 23:22:00 +0100

    update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function addNumber(number){
      document.getElementById('resultArea').innerHTML += number;
 }
 function calculate(){
-     let result = 1+1
+     let result = document.getElementById(resultArea)
      document.getElementById('resultArea').innerHTML = result;
 }
