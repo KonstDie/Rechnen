5382e8db8ceefa707a6c6e317d5613f88f2d595b
KonstDie <79140815+KonstDie@users.noreply.github.com>
2024-12-13 23:50:56 +0100

    update script.js

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ function calculate(){
 }
 function deleteAll(){
     let container = document.getElementById('resultArea');
-    container.innerHTML = container.innerHTML,slice(0,-1);
+    container.innerHTML = container.innerHTML.slice(0,-1);
 }
