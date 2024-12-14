053e70694568a23a1eda3454d975fc771cadd871
KonstDie <79140815+KonstDie@users.noreply.github.com>
2024-12-14 18:57:28 +0100

    update script.js

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function deleteLast(){
      let container = document.getElementById('resultArea');
      container.innerHTML = container.innerHTML.slice(0,-1);
 }
+
+document.addEventListener("keydown", function(event) {
+     if (event.key === "a") {
+          let number = event.key
+          document.getElementById('resultArea').innerHTML += number;
+         document.body.style.backgroundColor = "lightblue"; 
+     } else if (event.key === "Enter") {
+         alert("Enter wurde gedrückt!");
+     }
+ });
+ 
